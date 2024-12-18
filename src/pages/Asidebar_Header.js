015f1830d677119df9ea9c61f2c2a342f5e14d48
tamilvalavan <tamilvalavan.s@vdartinc.com>
2024-12-18 15:51:24 +0530

    updated asidebar
    Signed-off-by: tamilvalavan <tamilvalavan.s@vdartinc.com>

---
+++ src/pages/Asidebar_Header.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Header from '../components/header'
+import AsideBar from '../components/Asidebar/AsideBar';
+
+const CompanyCreation = () => {
+  
+  return (
+    <div><Header />
+    <AsideBar /></div>
+    
+  );
+};
+
+export default CompanyCreation;
